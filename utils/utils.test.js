3370363e35f31582b1bfe7f0c868de53da548479
Hamzakhann <hamzakhann66@gmail.com>
2018-10-16 13:33:08 +0500

    added assertion library and did some assertions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,16 +1,35 @@
 const utils = require('./utils')
-
+const expect = require('expect')
 it('should  add two numbers' , ()=>{
   const res = utils.add(33 , 11)
-  if(res !== 44){
-    throw new Error(`Wxpected 44 but got ${res}`)
-  }
+  expect(res).not.toBe(25)
+  expect(res).toBe(44)
+  expect(typeof res).toBe('number')
 
 })
 
 it('should square a number' , ()=>{
   const res = utils.square(5)
-  if(res!==25){
-    throw new Error(`expected 25 but got ${res}`)
-  }
+  expect(res).toBe(25)
+  expect(typeof res).toBe('number')
+})
+
+
+it('should set firstname and lastname' , ()=>{
+  let user = {location:'karachi' , age : 25}
+  let res = utils.setName(user , "Hamza Khan")
+
+  expect(res).toMatchObject({
+    firstname : 'Hamza',
+    lastname : 'Khan'
+  })
+})
+
+
+it('should expect some values' , ()=>{
+  expect({name:'hamza' , age:22}).toEqual({name:'hamza' , age : 22})
+  expect({name:'Hamza'}).not.toEqual({name:'hamza'})
+  expect([2,3,4]).toContain(3)
+  expect([1,2,3]).not.toContain(8)
+  expect({name:'hamza' , age : 25 , program:'bscs'}).toMatchObject({age:25})
 })
