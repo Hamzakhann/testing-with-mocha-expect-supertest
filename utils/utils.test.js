442bc608956d5de5518d02206b136e019223da40
Hamzakhann <hamzakhann66@gmail.com>
2018-10-16 12:09:18 +0500

    aded simple test with mocha

---
+++ utils/utils.test.js
@@ -0,0 +1,16 @@
+const utils = require('./utils')
+
+it('should  add two numbers' , ()=>{
+  const res = utils.add(33 , 11)
+  if(res !== 44){
+    throw new Error(`Wxpected 44 but got ${res}`)
+  }
+
+})
+
+it('should square a number' , ()=>{
+  const res = utils.square(5)
+  if(res!==25){
+    throw new Error(`expected 25 but got ${res}`)
+  }
+})
